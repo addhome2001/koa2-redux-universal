cd045188769df28c72946007fb2d768f1dce69cf
Seven <seven@megalive.com.tw>
2017-04-17 11:34:36 +0800

    Upgraded react version

--- src/common/containers/Profile.jsx
+++ src/common/containers/Profile.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes, PureComponent } from 'react';
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Btn from 'common/components/Btn';
 import styles from 'common/assets/css/app.css';
