2030cd8d4115536b331ddecf15b1f3206e62cf20
Seven <seven@megalive.com.tw>
2017-11-30 21:32:18 +0800

    Added loading component

--- src/common/components/Loadable.jsx
+++ src/common/components/Loadable.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
 import Loadable from 'react-loadable';
-// import Loading from './Loading';
+import Loading from './Loading';
 
 export default loader =>
   Loadable({
     loader,
     delay: 2000,
     timeout: 30000,
-    loading: () => <div>Loading...</div>,
-    // loading: <div>Loading...</div>,
+    loading: Loading,
   });
