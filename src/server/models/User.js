4f983df73c697a4ff31a25827af8c8b4e2ef988e
Seven <seven@megalive.com.tw>
2017-12-10 19:02:14 +0800

    redefine User model

---
+++ src/server/models/User.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, Sequelize) =>
+  sequelize.define('User', {
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    username: {
+      type: Sequelize.STRING,
+      notEmpty: true,
+    },
+    about: {
+      type: Sequelize.TEXT,
+    },
+    email: {
+      type: Sequelize.STRING,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    last_login: {
+      type: Sequelize.DATE,
+    },
+  });
