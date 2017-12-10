383e5bb1fa9c0a563f0887a87113063561dd6a7f
Seven <seven@megalive.com.tw>
2017-12-10 18:56:08 +0800

    migrate middlewares

---
+++ src/server/middlewares.js
@@ -0,0 +1,51 @@
+import path from 'path';
+import csrf from 'koa-csrf';
+import session from 'koa-session';
+import etag from 'koa-etag';
+import conditional from 'koa-conditional-get';
+import onerror from 'koa-onerror';
+import views from 'koa-views';
+import body from 'koa-bodyparser';
+import serve from 'koa-static';
+import passport from 'koa-passport';
+import logger from 'koa-logger';
+import redisStore from 'koa-redis';
+
+module.exports = (app, config) => {
+  // session
+  app.keys = ['secret1', 'secret2', 'secret3'];
+
+  // store session in redis or memory
+  app.use(session(config.SESSION_STORE && {
+    store: redisStore(config.SESSION_STORE),
+  }, app));
+
+  // body parser
+  app.use(body());
+
+  // passport
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  // view engine
+  app.use(views(path.resolve(__dirname, 'views'), { extension: 'ejs' }));
+
+  // static
+  app.use(serve(path.resolve(__dirname, 'static')));
+
+  app.use(conditional());
+  app.use(etag());
+
+  app.use(async (ctx, next) => {
+    ctx.body = ctx.request.body;
+    await next();
+  });
+
+  // enable csrf
+  app.use(csrf());
+
+  // logger
+  app.use(logger());
+
+  onerror(app);
+};
