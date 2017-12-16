009fa32711db1e726c515bdae512eb07a1cea581
Seven <seven@megalive.com.tw>
2017-12-16 22:38:00 +0800

    abstract SubmitController

--- src/common/containers/Login.jsx
+++ src/common/containers/Login.jsx
@@ -1,45 +1,12 @@
-import { bindActionCreators } from 'redux';
-import { connectAdvanced } from 'react-redux';
-
-import * as Actions from 'common/redux/modules/auth/actions';
-
 // Components
 import Login from 'common/components/Pages/Login';
 
-function selectorFactory(dispatch) {
-  const submitFormAction = bindActionCreators(Actions.loginAsync, dispatch);
-  const setFailureMessageAction = bindActionCreators(Actions.setFailureMessage, dispatch);
-  const actions = {
-    submitForm: csrf => (account) => {
-      if (!csrf) throw new Error('csrf token was not found.');
-      submitFormAction({ csrf, account });
-    },
-    setFailureMessage: setFailureMessageAction,
-  };
-  let csrfCache;
-  let result = {};
+import * as Actions from 'common/redux/modules/auth/actions';
 
-  return (nextState) => {
-    const { auth, csrf } = nextState;
-    const { loading = false, failureMessage = '' } = auth;
+import SubmitController from './SubmitController';
 
-    if (!csrfCache) {
-      csrfCache = csrf;
-      actions.submitForm = actions.submitForm(csrf);
-    }
-
-    const nextResult = {
-      loading,
-      failureMessage,
-      ...actions,
-    };
-
-    if (JSON.stringify(result) !== JSON.stringify(nextResult)) {
-      result = Object.assign({}, result, nextResult);
-    }
-
-    return result;
-  };
-}
-
-export default connectAdvanced(selectorFactory)(Login);
+export default SubmitController(
+  Actions.loginAsync,
+  Actions.setFailureMessage,
+  Login,
+);
