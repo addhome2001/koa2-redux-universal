b33f8b406f5859eeecf90b7eab5c0ab1da172f31
Seven <addhome2001@yahoo.com.tw>
2018-03-07 22:22:40 +0800

    client: extend the ErrorPage component

--- src/common/components/Pages/ErrorPage.jsx
+++ src/common/components/Pages/ErrorPage.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Btn from 'common/components/Elements/Btn';
 import Text from 'common/components/Elements/Text';
 
 const ErrorPage = () => (
-  <div>
-    <Text>Invalid Username or Password</Text>
-    <Btn to="/login">Login Again</Btn>
-  </div>
+  <React.Fragment>
+    <Switch>
+      <Route
+        exact
+        path="/error"
+        render={() => <Text>Something went wrong.</Text>}
+      />
+      <Route
+        path="/error/duplicateEmail/:email"
+        render={({ match }) => (
+          <Text>The email {match.params.email} has been took.</Text>
+        )}
+      />
+      <Route
+        path="/error/failedOauth/:provider"
+        render={({ match }) => (
+          <Text>
+            Something went wrong with your {match.params.provider}{' '}
+            authentication.
+          </Text>
+        )}
+      />
+    </Switch>
+    <Btn to="/">Back To Home</Btn>
+  </React.Fragment>
 );
 
 export default ErrorPage;
