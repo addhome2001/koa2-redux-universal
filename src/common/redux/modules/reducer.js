dc8bfee86a58896d43b59c09f35d9ad737261f63
Seven <addhome2001@yahoo.com.tw>
2018-03-07 22:29:27 +0800

    client: rename file

--- src/common/redux/modules/reducer.js
+++ src/common/redux/modules/reducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
-import auth from './auth/reducers';
+import auth from './auth/reducer';
 
 export default combineReducers({
   auth,
