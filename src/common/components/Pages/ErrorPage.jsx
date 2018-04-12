56809aad444dc665614277af778f29a2054a28db
Seven <addhome2001@yahoo.com.tw>
2018-04-12 20:35:56 +0800

    client: remove unused routes in the ErrorPage component

--- src/common/components/Pages/ErrorPage.jsx
+++ src/common/components/Pages/ErrorPage.jsx
@@ -11,21 +11,6 @@ const ErrorPage = () => (
         path="/error"
         render={() => <Text>Something went wrong.</Text>}
       />
-      <Route
-        path="/error/duplicateEmail/:email"
-        render={({ match }) => (
-          <Text>The email {match.params.email} has been took.</Text>
-        )}
-      />
-      <Route
-        path="/error/failedOauth/:provider"
-        render={({ match }) => (
-          <Text>
-            Something went wrong with your {match.params.provider}{' '}
-            authentication.
-          </Text>
-        )}
-      />
     </Switch>
     <Btn to="/">Back To Home</Btn>
   </React.Fragment>
