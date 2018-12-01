e938d9382a6caeed454f9f5367b395d655aeed01
Seven <addhome2001@yahoo.com.tw>
2018-12-01 17:12:33 +0800

    file renaming

--- src/server/index.js
+++ src/server/index.js
@@ -1,20 +1,20 @@
 import Koa from 'koa';
 import path from 'path';
 import Loadable from 'react-loadable';
-import middlewares from './middlewares';
+import applyMiddleware from './applyMiddleware';
 import config from './config';
 import { initLogger } from './core/utils/loggers';
 import render from './core/render';
 
 const app = new Koa();
 
-// common middlewares
-middlewares(app);
+// common middleware
+applyMiddleware(app);
 
 if (config.DEV) {
   // server hot reload for the core directory
   require('./config/serverWatcher')(path.resolve(__dirname, './core'));
-  // webpack dev middlewares
+  // webpack dev middleware
   app.use(require('./config/webpack'));
 }
 
